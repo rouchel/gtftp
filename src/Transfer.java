f0b057f7e8c2bf62b76b6204f9a212ded505f501
Alair Chen <365890515@qq.com>
2012-02-09 14:31:17 +0800

    add wrq retry and progressBar
    Signed-off-by: Alair Chen <365890515@qq.com>

--- src/Transfer.java
+++ src/Transfer.java
@@ -5,6 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.Arrays;
 
 class Transfer extends Tftp implements Runnable {
 
@@ -13,6 +14,7 @@ class Transfer extends Tftp implements Runnable {
 
 	private byte[] rcvBuffer;
 	private byte[] sndBuffer;
+	private byte[] dataBuffer;
 	private InetAddress destAddr;
 	private int destPort;
 	private short opcode;
@@ -21,6 +23,7 @@ class Transfer extends Tftp implements Runnable {
 	private String mod;
 	private long filesize;
 	private long sendsize;
+	private long recvsize;
 	protected String sendMsg;
 	protected int percent;
 
@@ -143,20 +146,66 @@ class Transfer extends Tftp implements Runnable {
 	private void wrq() throws IOException {
 		subSocket = new DatagramSocket();
 		subPacket = new DatagramPacket(sndBuffer, PKG_LEN, destAddr, destPort);
-		RandomAccessFile af = new RandomAccessFile(fileName, "rw");
+		subSocket.setSoTimeout(2000);
+
+		RandomAccessFile af = new RandomAccessFile(serverPath + fileName, "rw");
 
 		try {
 			short blknum = 0;
 			int len = 0;
 			while (true) {
 				setAck(sndBuffer, blknum);
-				sendPacket(subSocket, subPacket, sndBuffer, PKG_LEN);
-				len = receivePacket(subSocket, subPacket, rcvBuffer);
-				blknum = (short) getBlknum(rcvBuffer);
 
-				System.out.println(blknum);
+				blknum++;
 
-				af.write(rcvBuffer, 0, len);
+				for (int i = 0; i < RetryNum; i++) {
+					sendPacket(subSocket, subPacket, sndBuffer, PKG_LEN);
+
+					try {
+						len = receivePacket(subSocket, subPacket, rcvBuffer);
+					} catch (Exception e) {
+						System.out.println("rcvRetry: " + (i + 1));
+
+						if (i == RetryNum - 1) {
+							// System.out.println("Timeout!");
+							sendMsg = fileName + " <- "
+									+ destAddr.getHostAddress()
+									+ " Failed! (TimeOut!)";
+							progressMsg(false);
+							return;
+						}
+						continue;
+					}
+
+					switch (getOpcode(rcvBuffer)) {
+						case DAT :
+							if (getBlknum(rcvBuffer) == blknum)
+								i = RetryNum + 1;
+							break;
+						case ERR :
+							return;
+					}
+
+					dataBuffer = Arrays.copyOfRange(rcvBuffer, 4, PKG_LEN);
+					try {
+						af.write(dataBuffer, 0, len - 4);
+					} catch (Exception e) {
+						setError(sndBuffer, (short) 1,
+								"maybe the folder is full");
+						sendPacket(subSocket, subPacket, sndBuffer, PKG_LEN);
+						sendMsg = fileName + " -> " + destAddr.getHostAddress()
+								+ " Failed! (maybe the folder is full!)";
+						progressMsg(false);
+						subSocket.close();
+						return;
+					}
+
+					recvsize += len - 4;
+
+					sendMsg = fileName + "( " + Long.toString(recvsize) + " )"
+							+ " <- " + destAddr.getHostAddress();
+					progressMsg(true);
+				}
 
 				if (len < PKG_LEN - 4) {
 					break;
@@ -165,6 +214,10 @@ class Transfer extends Tftp implements Runnable {
 
 			setAck(sndBuffer, blknum);
 			sendPacket(subSocket, subPacket, sndBuffer, PKG_LEN);
+			sendMsg = fileName + "( " + Long.toString(recvsize) + " )" + " <- "
+					+ destAddr.getHostAddress() + " is done!";
+			progressMsg(true);
+
 		} finally {
 			subSocket.close();
 			af.close();
@@ -173,5 +226,4 @@ class Transfer extends Tftp implements Runnable {
 		System.out.println(fileName + " receive from " + destAddr
 				+ " is done!~");
 	}
-
 }
